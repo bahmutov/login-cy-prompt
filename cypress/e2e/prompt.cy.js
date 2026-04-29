0ac3304aabf2e7ed5daad10eff09707c6491bf15
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2026-04-28 21:23:52 -0400

    start the record spec

--- cypress/e2e/prompt.cy.js
+++ cypress/e2e/prompt.cy.js
@@ -21,18 +21,4 @@ describe('Login', () => {
       )
     })
   })
-
-  // it('fails to log in', () => {
-  //   cy.visit('public/index.html')
-  //   cy.get('.login-container').should('be.visible')
-  //   cy.get('#username').type(Cypress.expose('username'))
-  //   cy.get('#password').type('wrong-password', { log: false })
-  //   cy.contains('button', 'Login').click()
-  //   cy.get('.login-error').should(
-  //     'have.text',
-  //     'wrong username or password',
-  //   )
-  //   cy.get('.login-container').should('be.visible')
-  //   cy.get('.authenticated-message').should('not.exist')
-  // })
 })
---
+++ cypress/e2e/record.cy.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe('Login', () => {
+  it('logs in', () => {
+    cy.visit('public/index.html')
+  })
+})
