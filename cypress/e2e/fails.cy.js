da8563e20b409c6e4871565905fa8b8c95e3d061
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2026-04-28 21:52:18 -0400

    testing

--- cypress/e2e/fails.cy.js
+++ cypress/e2e/fails.cy.js
@@ -1,18 +1,17 @@
 /// <reference types="cypress" />
 
 describe('Login', () => {
-  it('logs in', () => {
+  it('fails to log in', () => {
     cy.visit('public/index.html')
     cy.get('.login-container').should('be.visible')
     cy.get('#username').type(Cypress.expose('username'))
-    cy.env(['password']).then(({ password }) => {
-      cy.get('#password').type(password, { log: false })
-    })
+    cy.get('#password').type('wrong-password', { log: false })
     cy.contains('button', 'Login').click()
-    cy.get('.login-container').should('not.exist')
-    cy.get('.authenticated-message').should(
+    cy.get('.login-error').should(
       'have.text',
-      'Authenticated',
+      'wrong username or password',
     )
+    cy.get('.login-container').should('be.visible')
+    cy.get('.authenticated-message').should('not.exist')
   })
 })
